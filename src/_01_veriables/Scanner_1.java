f5d0775812ca3eecb099b098bc39e930f213ce43
Abdulsamet Akbaba <bilinmezlerdenklemi@gmail.com>
2022-04-10 23:35:33 +0100

    first day

---
+++ src/_01_veriables/Scanner_1.java
@@ -0,0 +1,19 @@
+package _01_veriables;
+
+import java.util.Scanner;
+
+public class Scanner_1 {
+    public static void main(String[] args) {
+        /*
+        Kullanıcıdan bir sayı alın ve sayının küpünün yarısını konsola yazdıran bir program yazın.
+         */
+
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Sayi giriniz");
+        double sayi=scan.nextDouble();
+        double yeniSayi=(sayi*sayi*sayi)/2;
+        System.out.println(yeniSayi);
+
+
+    }
+}
---
+++ src/_01_veriables/Scanner_2.java
@@ -0,0 +1,26 @@
+package _01_veriables;
+
+import java.util.Scanner;
+
+public class Scanner_2 {
+    /*
+    Kullanıcıdan karenin kenar uzunluğunu alın ve karenin alanını ve çevresini konsola yazdıran bir program yazın.
+
+        Ornek Cikti :
+
+        Alan: 9
+
+        Cevre: 12
+     */
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Karenin kenarini giriniz");
+        double kenar=scan.nextDouble();
+
+        double cevre=kenar*4;
+        double alan=kenar*kenar;
+        System.out.println("cevre : " + cevre);
+        System.out.println("alan : " + alan);
+
+    }
+}
